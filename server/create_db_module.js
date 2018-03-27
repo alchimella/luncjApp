06d21a9596e05dcce0c364b0655b7778a2c60c9e
Abi <alchimella13@gmail.com>
2018-03-27 12:06:27 +0600

    Create and connect to mongoDB

---
+++ server/create_db_module.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw  err;
+    let dbo = db.db("lunchDB");
+
+    console.log("Database created!");
+    dbo.createCollection("places", function(err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
+        db.close();
+    });
+});
