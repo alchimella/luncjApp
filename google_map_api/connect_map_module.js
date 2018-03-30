60be5cf1f2340a3cb4198fdf97c43e30d5c4289e
Abi <alchimella13@gmail.com>
2018-03-30 16:59:22 +0600

    Create and connect to mongoDB

--- google_map_api/connect_map_module.js
+++ google_map_api/connect_map_module.js
@@ -1,88 +1,4 @@
-/*function myMap() {
-    let marker;
-    let clickedElement = document.getElementById('clicked_element');
-    // Google Maps API - прорисовка карты
-    let mapProp = {
-        center: new google.maps.LatLng(42.87909718755642, 74.5953816175461),
-        zoom: 16,
-        mapTypeControl: false,
-        streetViewControl: false
-    };
-    let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-    // Google Maps API - добавление и перемещение маркера при клике
-    map.addListener('click', function(event) {
-        addMarker(event.latLng);
-        clickedElement.innerHTML = 'карту, координаты ' + event.latLng;
-    });
-    addMarker(mapProp);
-
-    function addMarker(location) {
-        if (marker == null) {
-            marker = new google.maps.Marker({
-                position: location,
-                animation: google.maps.Animation.DROP,
-                map: map
-            });
-        } else {
-            marker.setPosition(location);
-        }
-    }
-
-    // Google Maps API - добавление события подсказки геолокации пользователя
-    let infoWindow = new google.maps.InfoWindow( {
-        map: map
-    });
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            let pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Вы тут!');
-            map.setCenter(pos);
-            map.data.add({ geometry: new google.maps.Data.Polygon(pos)});
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-
-    let geocoder = new google.maps.Geocoder;
-
-    document.getElementById('submit').addEventListener('click', function() {
-        geocodeLatLng(geocoder, map, infoWindow);
-    });
-}
-
-function geocodeLatLng(geocoder, map, infoWindow) {
-    geocoder.geocode({'placeId': map.latLng}, function(results, status) {
-        if (status === 'OK') {
-            if (results[0]) {
-                map.setZoom(11);
-                map.setCenter(results[0].geometry.location);
-                infowindow.setContent(results[0].formatted_address);
-                infowindow.open(map, marker);
-            } else {
-                window.alert('No results found');
-            }
-        } else {
-            window.alert('Geocoder failed due to: ' + status);
-        }
-    });
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-}*/
+let marker;
 function initMap() {
     let map = new google.maps.Map(document.getElementById('googleMap'), {
         center: new google.maps.LatLng(42.87909718755642, 74.5953816175461),
@@ -93,9 +9,20 @@ function initMap() {
 
     let geocoder = new google.maps.Geocoder();
 
+    map.addListener('click', function(e) {
+        geocodeAddress(e.latLng, map);
+    });
+
     document.getElementById('submit').addEventListener('click', function() {
         geocodeAddress(geocoder, map);
     });
+
+    // Google Maps API - автозаполнение поисковика
+    let input = /** @type {!HTMLInputElement} */(
+        document.getElementById('address'));
+
+    let autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
 
     // Google Maps API - добавление события подсказки геолокации пользователя
     let infoWindow = new google.maps.InfoWindow( {
@@ -133,12 +60,17 @@ function geocodeAddress(geocoder, resultsMap) {
     let address = document.getElementById('address').value;
     geocoder.geocode({'address': address}, function(results, status) {
         if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            let marker = new google.maps.Marker({
-                map: resultsMap,
-                position: results[0].geometry.location,
-                animation: google.maps.Animation.DROP
-            });
+            if (marker == null) {
+                resultsMap.setCenter(results[0].geometry.location);
+                marker = new google.maps.Marker({
+                    map: resultsMap,
+                    position: results[0].geometry.location,
+                    animation: google.maps.Animation.DROP
+                });
+            } else {
+                marker.setPosition(geocoder, resultsMap);
+            }
+
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
         }
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 class App extends Component {
   render() {
